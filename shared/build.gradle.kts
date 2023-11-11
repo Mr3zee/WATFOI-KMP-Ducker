71587afcbf711b3f199bb16e8c9e5303c8b7d2ea
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2023-11-11 22:14:57 +0100

    initial commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    id("org.jetbrains.kotlin.android")
+}
+
+kotlin {
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    jvm()
+    
+    sourceSets {
+        commonMain.dependencies {
+            // put your Multiplatform dependencies here
+        }
+    }
+}
+
+android {
+    namespace = "org.example.project.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:+")
+    implementation("androidx.core:core-ktx:+")
+}
